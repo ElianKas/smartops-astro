871ad9236b16af20707eaa6377dcf4d9d130277c
ElianKas <135838274+ElianKas@users.noreply.github.com>
2025-08-22 20:04:27 +0200

    first session

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,26 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import { storyblok } from '@storyblok/astro';
+import { loadEnv } from 'vite';
+
+const env = loadEnv('', process.cwd(), 'STORYBLOK');
+const { STORYBLOK_DELIVERY_API_TOKEN } = loadEnv(import.meta.env.MODE, process.cwd(), '');
 
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
+	integrations: [
+		storyblok({
+			accessToken: env.STORYBLOK_DELIVERY_API_TOKEN,
+			apiOptions: {
+				region: 'eu',
+			},
+			components: {
+				smartops_project: 'storyblok/smartops_project',
+			},
+		}),
+	],
 });
---
+++ src/utils/useSchemas.ts
@@ -0,0 +1,30 @@
+export interface Story {
+	name: string;
+    uuid: string;
+    slug: string;
+    full_slug: string;
+	content: SmartopsProject;
+}
+
+export interface SmartopsProject {
+	project_image: SmartopsProjectImage;
+    project_title: string;
+    project_sections: Array<SmartopsProjectImageSection | SmartopsProjectTextSection>;
+    project_categories: Array<string>;
+}
+
+export interface SmartopsProjectImageSection {
+    component: "project_section_assets";
+    section_assets: Array<SmartopsProjectImage>;
+}
+
+export interface SmartopsProjectImage {
+    id: number;
+    alt: string;
+    filename: string;
+}
+
+export interface SmartopsProjectTextSection {
+    component: "project_section_text";
+    section_richtext: object;
+}
