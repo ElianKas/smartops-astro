6d3eba1623b9766507c436137d16f9d5dccc9b95
ElianKas <135838274+ElianKas@users.noreply.github.com>
2025-08-24 19:10:39 +0200

    style: categories

---
+++ src/utils/useLists.ts
@@ -0,0 +1,34 @@
+export const projectCategories = [
+	//value is matching the storyblok project_categories value
+
+	// Technologien
+	{ group: 'Technologien', name: 'Solar', value: 'technology_solar' },
+	{ group: 'Technologien', name: 'Wind', value: 'technology_wind' },
+	{ group: 'Technologien', name: 'Speicher', value: 'technology_speicher' },
+	{ group: 'Technologien', name: 'ESYS', value: 'technology_esys' },
+	{ group: 'Technologien', name: 'eMobil', value: 'technology_emobil' },
+
+	// Sparring
+	{ group: 'Sparring', name: 'Management Consulting', value: 'sparring_mc' },
+	{ group: 'Sparring', name: 'Advisory', value: 'sparring_advisory' },
+	{ group: 'Sparring', name: 'Review & 2nd Opinion', value: 'sparring_review' },
+	{ group: 'Sparring', name: 'Coaching', value: 'sparring_coaching' },
+
+	// Projekte & Lösungen
+	{ group: 'Projekte & Lösungen', name: 'Konzeptstudien', value: 'pl_konzeptstudien' },
+	{
+		group: 'Projekte & Lösungen',
+		name: 'Potential- & Ertragsstudien',
+		value: 'pl_potentialstudien',
+	},
+	{ group: 'Projekte & Lösungen', name: 'Umweltstudien', value: 'pl_umweltstudien' },
+	{ group: 'Projekte & Lösungen', name: 'Machbarkeitsstudien', value: 'pl_machbarkeitsstudien' },
+	{ group: 'Projekte & Lösungen', name: 'Engineering & Design (Entwurf)', value: 'pl_engineering' },
+	{
+		group: 'Projekte & Lösungen',
+		name: 'Spezifikation & Ausschreibung',
+		value: 'pl_spezifikation',
+	},
+	{ group: 'Projekte & Lösungen', name: 'Baubetreuung', value: 'pl_baubetreuung' },
+	{ group: 'Projekte & Lösungen', name: 'Audits & Abnahme', value: 'pl_audits' },
+];
